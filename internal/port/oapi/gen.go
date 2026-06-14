2f5dd4508e16962d996837ef4ed53898a6d4f911
Malik Benkirane <mbenkirane@internetworks.blog>
2026-06-14 12:12:00 +0100

    feat(oapi): generate strict server interface and types

--- internal/port/oapi/gen.go
+++ internal/port/oapi/gen.go
@@ -1,4 +1,4 @@
 package oapi
 
-//go:generate go tool oapi-codegen -config cfg.yaml ../../openapi.json
-//go:generate go tool oapi-codegen -config strict.cfg.yaml ../../openapi.json
+//go:generate go tool oapi-codegen -config cfg.yaml ../../../openapi.json
+//go:generate go tool oapi-codegen -config strict.cfg.yaml ../../../openapi.json
